12c887d4b6d1e70659c008b709c543c864f009c4
Noel Sebu <noelsebu555@gmail.com>
2025-05-17 15:06:31 +0530

    get name and gmail to display from context

---
+++ app/home/page.tsx
@@ -0,0 +1,89 @@
+"use client";
+import { Context } from "@/ContextAPI";
+import Image from "next/image";
+import React, { useContext } from "react";
+import { GoChevronRight } from "react-icons/go";
+
+const page = () => {
+  const { name, gmail } = useContext(Context);
+
+  return (
+    <div className="flex">
+      <div className="">
+        <Image
+          src="/flowers.avif"
+          alt="Image"
+          className="h-screen w-[50vw]"
+          width={1000}
+          height={1000}
+          loading="eager"
+        />
+      </div>
+      <div className="w-[50vw] mt-6 flex flex-col items-center">
+        <div className="w-[36rem] flex items-start">
+          <h1 className="text-6xl">Hello!</h1>
+        </div>
+        <div className="w-[36rem] h-[23rem] mt-6 shadow-lg rounded-lg">
+          <div className="flex items-center gap-5 m-4">
+            <div className="h-24 w-24 text-3xl bg-blue-400 rounded-full grid items-center justify-center">
+              Y
+            </div>
+            <div>
+              <div className="text-xl">{name}</div>
+              <div>{gmail}</div>
+            </div>
+          </div>
+          <hr className="border-2 ml-3 rounded-2xl border-zinc-300 w-[53%]" />
+          <div className="space-y-3 mt-3 ml-3">
+            <div className="h-10 w-[19rem] p-1 flex items-center justify-between">
+              <h1>My Profile</h1>
+              <GoChevronRight size={21} className="translate-y-0.5" />
+            </div>
+            <div className=" h-10 w-[19rem] p-1 flex items-center justify-between">
+              <h1>Settings</h1>
+              <GoChevronRight size={21} className="translate-y-0.5" />
+            </div>
+            <div className=" h-10 w-[19rem] p-1 flex items-center justify-between">
+              <h1>Notification</h1>
+              <h1>Allow</h1>
+            </div>
+            <div className=" h-10 w-[19rem] p-1 flex items-center justify-between">
+              <h1>Log Out</h1>
+            </div>
+          </div>
+        </div>
+        <div className="w-[36rem] h-[9rem] mt-5 flex flex-col items-center justify-center gap-1 rounded-lg shadow-xl">
+          <div className="h-10 w-[19rem] p-1 flex items-center justify-between">
+            <h1 className="text-lg">Settings</h1>
+            <GoChevronRight size={21} className="translate-y-0.5" />
+          </div>
+          <hr className="border-2 rounded-2xl border-zinc-300 w-[50%]" />
+          <div className="">
+            <div className="h-8 w-[19rem] p-1 flex items-center justify-between">
+              <h1>Theme</h1>
+              <div className="flex gap-2">
+                <h1 className="text-sm">Light</h1>
+                <GoChevronRight
+                  size={21}
+                  className="translate-y-0.5 rotate-90"
+                />
+              </div>
+            </div>
+            <div className=" h-8 w-[19rem] p-1 flex items-center justify-between">
+              <h1>Language</h1>
+              <div className="flex gap-2">
+                <h1 className="text-sm">Eng</h1>
+                <GoChevronRight
+                  size={21}
+                  className="translate-y-0.5 rotate-90"
+                />
+              </div>
+            </div>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default page;
