63ab6949df56ec4f8acbf677f1b54fd008891124
Noel Sebu <noelsebu555@gmail.com>
2025-05-17 15:32:26 +0530

    types set and configured

--- ContextAPI/index.tsx
+++ ContextAPI/index.tsx
@@ -1,12 +1,25 @@
 "use client";
-import { createContext, useState } from "react";
+import { createContext, ReactNode, useState } from "react";
 
-export const Context = createContext();
+export interface UserContextType {
+  name: string;
+  setname: (name: string) => void;
+  gmail: string;
+  setgmail: (gmail: string) => void;
+  password: string;
+  setpassword: (password: string) => void;
+}
 
-export const ContextProvider = ({ children }) => {
-  const [name, setname] = useState("");
-  const [gmail, setgmail] = useState("");
-  const [password, setpassword] = useState("")
+interface ContextProviderProps {
+  children: ReactNode;
+}
+
+export const Context = createContext<UserContextType | null>(null);
+
+export const ContextProvider = ({ children }: ContextProviderProps) => {
+  const [name, setname] = useState<string>("");
+  const [gmail, setgmail] = useState<string>("");
+  const [password, setpassword] = useState<string>("")
 
   return (
     <Context.Provider value={{ name, setname, gmail, setgmail, password, setpassword }}>
