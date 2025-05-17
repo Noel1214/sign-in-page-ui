ea9285ad136a109b2989578fb999a80fd958983d
Noel Sebu <noelsebu555@gmail.com>
2025-05-17 15:06:53 +0530

    set data in context api based on input

--- components/InputBox.tsx
+++ components/InputBox.tsx
@@ -1,8 +1,21 @@
-// app/login/page.jsx (or app/signup/page.jsx)
 "use client";
 import Link from "next/link";
+import { Context } from "@/ContextAPI";
+import { useContext } from "react";
+import { useRouter } from "next/navigation";
 
 export default function LoginPage() {
+  const router = useRouter();
+  const { setname, setgmail, setpassword, name, gmail, password } = useContext(Context);
+
+  const handleSignUp = () => {
+    if(name === "") return;
+    if(gmail === "") return;
+    if(password === "") return;
+
+    router.push("/home")
+  };
+
   return (
     <div className="flex flex-col items-center justify-center space-y-16 mt-6 px-4 sm:px-6 lg:px-8">
       <div className="w-[91%]">
@@ -24,6 +37,7 @@ export default function LoginPage() {
                 type="text"
                 required
                 className="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-xl focus:outline-none"
+                onChange={(e) => setname(e.target.value)}
               />
             </div>
 
@@ -41,6 +55,7 @@ export default function LoginPage() {
                 autoComplete="email"
                 required
                 className="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-xl focus:outline-none"
+                onChange={(e) => setgmail(e.target.value)}
               />
             </div>
 
@@ -58,6 +73,7 @@ export default function LoginPage() {
                 autoComplete="current-password"
                 required
                 className="mt-1 block w-full px-3 py-1 border border-gray-300 rounded-xl focus:outline-none"
+                onChange={(e) => setpassword(e.target.value)}
               />
             </div>
 
@@ -84,7 +100,8 @@ export default function LoginPage() {
           <div>
             <button
               type="submit"
-              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-800 focus:outline-none"
+              className="w-full flex justify-center py-2 px-4 border border-transparent rounded-md shadow-sm text-sm font-medium text-white bg-green-800 focus:outline-none hover:cursor-pointer"
+              onClick={handleSignUp}
             >
               Sign up
             </button>
