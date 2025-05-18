54b0df2010d3ae7601af538f1ca30bc8493ed286
Noel Sebu <noelsebu555@gmail.com>
2025-05-18 14:24:52 +0530

    new state created to keep checkbox state

--- ContextAPI/index.tsx
+++ ContextAPI/index.tsx
@@ -8,6 +8,8 @@ export interface UserContextType {
   setgmail: (gmail: string) => void;
   password: string;
   setpassword: (password: string) => void;
+  agreeToTerms: boolean;
+  setagreeToTerms: (agreeToTerms: boolean) => void;
 }
 
 interface ContextProviderProps {
@@ -19,10 +21,22 @@ export const Context = createContext<UserContextType | null>(null);
 export const ContextProvider = ({ children }: ContextProviderProps) => {
   const [name, setname] = useState<string>("");
   const [gmail, setgmail] = useState<string>("");
-  const [password, setpassword] = useState<string>("")
+  const [password, setpassword] = useState<string>("");
+  const [agreeToTerms, setagreeToTerms] = useState<boolean>(false);
 
   return (
-    <Context.Provider value={{ name, setname, gmail, setgmail, password, setpassword }}>
+    <Context.Provider
+      value={{
+        name,
+        setname,
+        gmail,
+        setgmail,
+        password,
+        setpassword,
+        agreeToTerms,
+        setagreeToTerms,
+      }}
+    >
       {children}
     </Context.Provider>
   );
